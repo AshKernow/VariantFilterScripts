0ef84e27ff0473ddbf8bd1bb349d1c4451f85eb7
Ashley Sawle <ads2202@c2b2.columbia.edu>
2015-03-19 11:15:14 -0400

    Added functionality to allow for 0 for missing parent. Added variant annotation.

--- xCompoundHetFilters.sh
+++ xCompoundHetFilters.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #$ -cwd -l mem=1G,time=1:: -N FilterCHet
 
-#hpc workarounds
-if [[ /bin/hostname==*.hpc ]]; then 
-source /etc/profile.d/sge.sh  # SGE commands from within node
-source /ifs/home/c2b2/af_lab/ads2202/.bash_profile
-fi
+
+## use "0" for missing parent
 
 #get arguments
 while getopts v:t:n:p: opt; do
@@ -23,34 +20,53 @@ FiltScrDir="/ifs/scratch/c2b2/af_lab/ads2202/Exome_Seq/scripts/Filtering_scripts
 VcfFil=`readlink -f $VcfFil`
 FamFil=`readlink -f $FamFil`
 
-FamNam=`cut -f 1 $FamFil | head -n $SGE_TASK_ID | tail -n 1`
-ModNam=`cut -f 2 $FamFil | head -n $SGE_TASK_ID | tail -n 1`
-PatNam=`cut -f 3 $FamFil | head -n $SGE_TASK_ID | tail -n 1`
-MatNam=`cut -f 4 $FamFil | head -n $SGE_TASK_ID | tail -n 1`
-FilPrm=`cut -f 5 $FamFil | head -n $SGE_TASK_ID | tail -n 1`
+if [[ "$SGE_TASK_ID" != "undefined" ]]; then
+        ArrNum=$SGE_TASK_ID
+else
+        ArrNum=1
+fi
+
+FamNam=`cut -f 1 $FamFil | head -n $ArrNum | tail -n 1`
+ModNam=`cut -f 2 $FamFil | head -n $ArrNum | tail -n 1`
+PatNam=`cut -f 3 $FamFil | head -n $ArrNum | tail -n 1`
+MatNam=`cut -f 4 $FamFil | head -n $ArrNum | tail -n 1`
+FilPrm=`cut -f 5 $FamFil | head -n $ArrNum | tail -n 1`
 
 if [[ $FamNam == [0-9]* ]]; then FamNam=Fam$FamNam; fi
 
-DirNam=$FamNam.Fam
+DirNam=$FamNam
 if [[ -n $DirPre ]]; then DirNam=$DirPre"_"$DirNam; fi
 mkdir -p $DirNam
 cd $DirNam
 
-PatPrm=`echo $FilPrm | sed s/"--het "/"--het $PatNam,"/`
-if [[ "$PatPrm" == *--ref* ]]; then
-    PatPrm=`echo $PatPrm | sed s/"--ref "/"--ref $MatNam,"/`
-else
-    PatPrm=$PatPrm" --ref "$MatNam
+#get paternally interited allele
+PatPrm=$FilPrm
+if [[ "$PatNam" != "0" ]]; then 
+    PatPrm=`echo $PatPrm | sed s/"--het "/"--het $PatNam,"/`
+fi
+if [[ "$MatNam" != "0" ]]; then 
+    if [[ "$PatPrm" == *--ref* ]]; then
+        PatPrm=`echo $PatPrm | sed s/"--ref "/"--ref $MatNam,"/`
+    else
+        PatPrm=$PatPrm" --ref "$MatNam
+    fi
 fi
 CMD="$FiltScrDir/ExmFilt.CustomGenotype.py -v $VcfFil -o $FamNam.tempheppat $PatPrm"
 if [[ ! -z $AddPrm ]]; then CMD=$CMD" $AddPrm"; fi
 echo $CMD
 eval $CMD
-MatPrm=`echo $FilPrm | sed s/"--het "/"--het $MatNam,"/`
-if [[ "$MatPrm" == *--ref* ]]; then
-    MatPrm=`echo $MatPrm | sed s/"--ref "/"--ref $MatNam,"/`
-else
-    MatPrm=$MatPrm" --ref "$PatNam
+
+
+MatPrm=$FilPrm
+if [[ "$MatNam" != "0" ]]; then 
+    MatPrm=`echo $MatPrm | sed s/"--het "/"--het $MatNam,"/`
+fi
+if [[ "$PatNam" != "0" ]]; then 
+    if [[ "$MatPrm" == *--ref* ]]; then
+        MatPrm=`echo $MatPrm | sed s/"--ref "/"--ref $PatNam,"/`
+    else
+        MatPrm=$MatPrm" --ref "$PatNam
+    fi
 fi
 CMD="$FiltScrDir/ExmFilt.CustomGenotype.py -v $VcfFil -o $FamNam.temphepmat $MatPrm"
 if [[ ! -z $AddPrm ]]; then CMD=$CMD" $AddPrm"; fi
@@ -69,6 +85,9 @@ comphet <- rbind(mathet, pathet)
 comphet <- comphet[order(comphet[,"Chromosome"], comphet[,"Position"]),]
 write.table(comphet, "$FamNam.compound_heterozygous.tsv", col.names=T, row.names=F, quote=F, sep="\t")
 RSCRIPT
-cat $FamNam.tempheppat.log $FamNam.temphepmat.log > $FamNam.filter.compound_heterozygous.log
+cat $FamNam.tempheppat.log $FamNam.temphepmat.log > $FamNam.compound_heterozygous.log
 rm -rf *temphep*
-
+LEN=`cat $FamNam.compound_heterozygous.tsv | wc -l`
+if [[ $LEN -gt 1 ]]; then
+    qsub $FiltScrDir/xAnnotateVariantTSV.sh -i $FamNam.compound_heterozygous.tsv
+fi
