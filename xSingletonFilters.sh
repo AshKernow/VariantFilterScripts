39c0671f628eb76f4091c6997593b542638246d9
AshKernow <ads2202cu@gmail.com>
2015-11-03 10:08:33 -0500

    Fixed to work on powergen

--- xSingletonFilters.sh
+++ xSingletonFilters.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 #$ -cwd -l mem=1G,time=1:: -N FilterSing
 
-#hpc workarounds
-if [[ /bin/hostname==*.hpc ]]; then 
-source /etc/profile.d/sge.sh  # SGE commands from within node
-source /ifs/home/c2b2/af_lab/ads2202/.bash_profile
-fi
-
+ArrNum=1
 #get arguments
-while getopts v:t:n:p: opt; do
+while getopts v:t:n:a:p: opt; do
     case "$opt" in
         v) VcfFil="$OPTARG";;
         t) SingFil="$OPTARG";;
         p) AddPrm="$OPTARG";;
         n) DirPre="$OPTARG";;
+        a) ArrNum="$OPTARG";;
         #H) echo "$usage"; exit;;
     esac
 done
 
-FiltScrDir="/ifs/scratch/c2b2/af_lab/ads2202/Exome_Seq/scripts/Filtering_scripts/"
+FiltScrDir="/home/local/ARCS/ads2202/scripts/Filtering_scripts"
 
 VcfFil=`readlink -f $VcfFil`
 SingFil=`readlink -f $SingFil`
 
-Proband=`cut -f 2 $SingFil | head -n $SGE_TASK_ID | tail -n 1`
-FamNam=`cut -f 1 $SingFil | head -n $SGE_TASK_ID | tail -n 1`
+Proband=`cut -f 2 $SingFil | head -n $ArrNum | tail -n 1`
+FamNam=`cut -f 1 $SingFil | head -n $ArrNum | tail -n 1`
 
 if [[ $FamNam == [0-9]* ]]; then FamNam=Fam$FamNam; fi
 
@@ -41,7 +37,7 @@ echo $CMD
 eval $CMD
 LEN=`cat $Proband.AR.tsv | wc -l`
 if [[ $LEN -gt 1 ]]; then
-    qsub $FiltScrDir/xAnnotateVariantTSV.sh -i $Proband.AR.tsv
+    nohup $FiltScrDir/xAnnotateVariantTSV.sh -i $Proband.AR.tsv &
 fi
 
 #Autosomal Dominant
@@ -52,7 +48,7 @@ echo $CMD
 eval $CMD
 LEN=`cat $Proband.AD.tsv | wc -l`
 if [[ $LEN -gt 1 ]]; then
-    qsub $FiltScrDir/xAnnotateVariantTSV.sh -i $Proband.AD.tsv
+    nohup $FiltScrDir/xAnnotateVariantTSV.sh -i $Proband.AD.tsv &
 fi
 
 #Autosomal Dominant
@@ -74,6 +70,6 @@ write.table(comphet, "$Proband.compound_heterozygous.tsv", col.names=T, row.name
 RSCRIPT
 LEN=`cat $Proband.compound_heterozygous.tsv | wc -l`
 if [[ $LEN -gt 1 ]]; then
-    qsub $FiltScrDir/xAnnotateVariantTSV.sh -i $Proband.compound_heterozygous.tsv
+    nohup $FiltScrDir/xAnnotateVariantTSV.sh -i $Proband.compound_heterozygous.tsv &
 fi
 rm $Proband.tempcHet*
